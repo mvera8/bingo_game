1398f17b368b1a466f955f965f788893d3db3e43
tinchobolso8 <tinchobolso8@gmail.com>
2020-03-27 09:01:41 -0300

    logo

--- logo.php
+++ logo.php
@@ -1,7 +1,7 @@
 <a class="navbar-brand header__brand" href="index.php">
     <div class="site__logo">
 		<h1 class="site__title">Bingo!</h1>
-		<h1 class="site__title site__title--header">Bingo!</h1>
+		<img class="img--block" src="logo.svg" alt="Bingo | Mar&Mar" />
 		<div class="site__en-casa"></div>
 	</div>
 </a>
